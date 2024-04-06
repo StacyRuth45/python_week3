928d055881a04e4b7ca203004c40b9ac764fa619
Stacy Wangui Mwangi <160638172+StacyRuth45@users.noreply.github.com>
2024-04-06 16:36:40 +0300

    Add files via upload

---
+++ week3.py
@@ -0,0 +1,21 @@
+
+def calculate_discount(price, discount_percent):
+    final_price = price - discount_percent
+    if discount_percent >= 20%
+      discount_amount = price * (discount_percent / 100)
+      print(final_price)
+      return final_price
+    else:
+        return price
+        
+
+if __name__ == "__main__":
+   original_price = float(input("Enter the original prize of the iteam": $))
+   discount = float(input("Enter the discount": $))
+                          
+                          final_price = calculate_discount(original_price, discount)
+                          if final_price == original_price
+                          print(f"No discount applied. The Final Price is: ${final_price}")
+
+                          else:
+                          print(f"Discount applied{discount}% discount: $ {final_price}")
